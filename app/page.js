71ff037f41f81929204a4fc2ab434a6670ff8b14
Ashish Kumar Singh <kumarvns130@gmail.com>
2023-06-25 11:11:35 +0530

    Api error handling

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
+export const dynamic = 'force-dynamic';
+
 import Results from "./Results";
 
 const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
 
-  const genre = searchParams.genre || 'trending';
+  const genre = searchParams || 'trending';
 
   const res = await fetch(`https://api.themoviedb.org/3/${genre === 'topRated' ? 'movie/top_rated' :
     'trending/all/week'}?api_key=${API_KEY}&language=en-US&page=1`, { next: { revalidate: 10000 } });
